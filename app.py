b0ccadacf7901fdbadd61d3f36c6885c7f48652d
Oktavian Putra Masyiakh <78895409+oktavianpm@users.noreply.github.com>
2023-05-16 16:46:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ st.set_page_config(
 st.title("SmartFishSense")
 
 # --- HIDE STREAMLIT STYLE ---
-# hide_st_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # --- NAVIGATION MENU ---
 selected = option_menu(
