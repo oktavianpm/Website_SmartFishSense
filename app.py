a2046b64df47ef856164ca6d83b1c48d7ef988e8
Oktavian Putra Masyiakh <78895409+oktavianpm@users.noreply.github.com>
2023-05-16 16:23:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pymongo
+import requests
 
 from pymongo import MongoClient
 from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
@@ -61,8 +62,8 @@ def add_data():
     # coll = db.data1
     # # coll.drop()
 
-    timex = datetime.today()
-    # timex = timex + timedelta(hours=7) #khusus buat di github
+    # timex = datetime.today()
+    timex = timex + timedelta(hours=7) #khusus buat di github
     docs = [
             {"Date": (timex.strftime("%x")),"Time":  (timex.strftime("%X")), "Reset": resetx, "Exit_idle": exitx, "Status": statusx, "Confidence": confidencex,},
             ]
@@ -78,6 +79,20 @@ def load_data():
     selected_columns = ['Date','Time','Status','Confidence']
     df_selected = df[selected_columns]
     return df_selected
+
+# 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
+API_KEY = st.secrets["API_KEY"]
+API_URL = "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+# output = query({
+#     "inputs": "Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!",
+#     "parameters": {"candidate_labels": ["refund", "legal", "faq"]},
+# })
 
 if selected == "Control Menu":
     with col1:
